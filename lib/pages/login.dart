6468b9eb3bb0e3fa11ef777fd33010440d024e53
Mohd Mujtaba <mujtaba.aamir1@gmail.com>
2021-12-26 02:18:07 +0530

    day-6 stateful widget, animatedContainer, future delay

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,8 +1,16 @@
 import 'package:catelog/routes.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool buttonPressed = false;
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +26,9 @@ class LoginPage extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
-              const Text(
-                "Welcome! Login to proceed further",
-                style: TextStyle(
+              Text(
+                "Welcome ${name}",
+                style: const TextStyle(
                   fontSize: 30,
                   fontWeight: FontWeight.bold,
                   color: Colors.black,
@@ -34,10 +42,15 @@ class LoginPage extends StatelessWidget {
                     const EdgeInsets.symmetric(vertical: 20, horizontal: 60),
                 child: Column(children: [
                   TextFormField(
-                      decoration: const InputDecoration(
-                    labelText: "Username",
-                    hintText: "Enter your username",
-                  )),
+                    decoration: const InputDecoration(
+                      labelText: "Username",
+                      hintText: "Enter your username",
+                    ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
+                  ),
                   TextFormField(
                       obscureText: true,
                       decoration: const InputDecoration(
@@ -47,16 +60,40 @@ class LoginPage extends StatelessWidget {
                   const SizedBox(
                     height: 20,
                   ),
-                  ElevatedButton(
-                    style: TextButton.styleFrom(minimumSize: Size(100, 40)),
-                    onPressed: () {
-                      Navigator.pushNamed(context, MyRoute.homeRoute);
-                    },
-                    child: const Text(
-                      "Login",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
+                  InkWell(
+                      onTap: () async {
+                        buttonPressed = true;
+                        setState(() {});
+                        await Future.delayed(const Duration(seconds: 1));
+                        Navigator.pushNamed(context, MyRoute.homeRoute);
+                      },
+                      child: AnimatedContainer(
+                        duration: const Duration(seconds: 1),
+                        height: 40,
+                        width: buttonPressed ? 50 : 100,
+                        child: buttonPressed
+                            ? Icon(Icons.done)
+                            : const Text("Login",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                )),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: buttonPressed ? Colors.green : Colors.blue,
+                            borderRadius: BorderRadius.circular(50)),
+                      ))
+
+                  // ElevatedButton(
+                  //   style: TextButton.styleFrom(minimumSize: Size(100, 40)),
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRoute.homeRoute);
+                  //   },
+                  //   child: const Text(
+                  //     "Login",
+                  //     style: TextStyle(fontWeight: FontWeight.bold),
+                  //   ),
+                  // ),
                 ]),
               )
             ],
